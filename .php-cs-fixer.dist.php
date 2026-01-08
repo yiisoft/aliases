e6a5e9b77779f710ea06d732e99de1369da1dca9
Максим Спирков <spirkov.2001@mail.ru>
2026-01-08 16:25:53 +0400

    configs + deps

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+use PhpCsFixer\Config;
+use PhpCsFixer\Finder;
+use PhpCsFixer\Runner\Parallel\ParallelConfigFactory;
+
+$finder = (new Finder())->in([
+    __DIR__ . '/src',
+    __DIR__ . '/tests',
+]);
+
+return (new Config())
+    ->setParallelConfig(ParallelConfigFactory::detect())
+    ->setRules([
+        '@PER-CS3.0' => true,
+        'no_unused_imports' => true,
+        'ordered_class_elements' => true,
+        'class_attributes_separation' => ['elements' => ['method' => 'one']],
+        'fully_qualified_strict_types' => [
+            'import_symbols' => true
+        ],
+        'global_namespace_import' => [
+            'import_classes' => true,
+            'import_constants' => true,
+            'import_functions' => true,
+        ],
+    ])
+    ->setFinder($finder);
